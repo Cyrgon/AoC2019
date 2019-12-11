60cb3b34b4fb2222272b825edeae423343c87899
Lukas Gustavsson <lukas.gustavsson@workmobile.se>
2019-12-11 10:56:32 +0100

    Day 11

---
+++ 11/11.py
@@ -0,0 +1,60 @@
+import intcode as ic
+
+with open("input.txt") as f:
+    list = f.readline().strip().split(",")
+    program = dict([(x, int(list[x])) for x in range(0, len(list))])
+
+    dirs = ["up", "right", "down", "left"]
+    d = { "up": (0, -1), "right": (1, 0), "down": (0, 1), "left": (-1, 0)}
+    dir = 0
+
+    paint = {}
+
+    done = False
+    ip = 0
+    rb = 0
+    input = [1]
+
+    x = 0
+    y = 0
+    minX = 0
+    maxX = 0
+    minY = 0
+    maxY = 0
+
+    while not done:
+        (done, output, program, ip, rb) = ic.evaluate(program, input, ip, rb)
+
+        paint[(x, y)] = output[0]
+
+        if output[1] == 1:
+            dir += 1
+        else:
+            dir -= 1
+
+        nd = dirs[dir % 4]
+
+        if x < minX:
+            minX = x
+        if x > maxX:
+            maxX = x
+        if y < minY:
+            minY = y
+        if y > maxY:
+            maxY = y
+
+        x += d[nd][0]
+        y += d[nd][1]
+
+        if (x, y) in paint:
+            input.append(paint[(x, y)])
+        else:
+            input.append(0)
+
+    for i in range(minY, maxY + 1):
+        for j in range(minX, maxX + 1):
+            if (j, i) in paint and paint[(j, i)] == 1:
+                print "O",
+            else:
+                print " ",
+        print ""
